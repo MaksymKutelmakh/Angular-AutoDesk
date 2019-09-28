826e820fc00fc4367bc5e7704764269351a33226
MaksymKutelmakh <50178880+MaksymKutelmakh@users.noreply.github.com>
2019-09-28 20:53:52 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForgeViewerComponent } from './forge-viewer/forge-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForgeViewerComponent
   ],
   imports: [
     BrowserModule,
